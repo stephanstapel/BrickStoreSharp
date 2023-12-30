7e45cad3499be9c120cf4ffbfe04508170b2f409
Stephan <stephan.stapel@web.de>
2023-12-30 23:08:31 +0100

    added test project

---
+++ BrickStoreSharp-Test/Sample.cs
@@ -0,0 +1,13 @@
+namespace BrickStoreSharp_Test
+{
+    [TestClass]
+    public class Sample
+    {
+        [TestMethod]
+        public void Sample()
+        {
+            // Sample test
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
